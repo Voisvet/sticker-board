ecc04e4549c7228ec60e88cc6248de3eebfdfb8d
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2018-11-04 11:12:55 +0300

    add login form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,8 +3,45 @@ import { Redirect, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Cookies from 'js-cookie';
 
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
 import * as actions from '../../store/user/actions';
 import * as selectors from '../../store/user/reducer';
+
+// --------------------------------------------------
+//
+//  Styles section
+//
+// --------------------------------------------------
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: '10%'
+  },
+  inputField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 300
+  },
+  loginButton: {
+    marginTop: 24,
+    width: 300
+  }
+})
+
+// --------------------------------------------------
+//
+//  Component section
+//
+// --------------------------------------------------
+
 
 class Login extends React.Component {
   constructor(props) {
@@ -13,17 +50,47 @@ class Login extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
+
+    // If token exists, then user has already logged in,
+    // then just redirect to the app part
+    // In other case render login form
     return this.props.token ? (
       <Redirect to="/app" />
     ) : (
-      <div>
-        <p>Hello from login</p>
-        <Link to='/app'>Login</Link>
-        <p>{this.props.token}</p>
+      <div className={classes.container}>
+          <Typography variant="h6" color="inherit">
+            TG Bot CP
+          </Typography>
+          <TextField
+            id="outlined-username"
+            className={classes.inputField}
+            label="Username"
+            margin="normal"
+          />
+          <TextField
+            id="outlined-password"
+            className={classes.inputField}
+            label="Password"
+            margin="normal"
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.loginButton}
+          >
+            Log In
+          </Button>
       </div>
     );
   }
 }
+
+// --------------------------------------------------
+//
+//  State-to-props mapping and export section
+//
+// --------------------------------------------------
 
 const mapStateToProps = (state) => {
   return {
@@ -31,4 +98,5 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Login);
+const styledLogin = withStyles(styles)(Login);
+export default connect(mapStateToProps)(styledLogin);
