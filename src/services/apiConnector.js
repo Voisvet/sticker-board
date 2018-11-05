75bd6fcd52cdc9fb6f890b11b400ec1e94f934c9
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2018-11-05 10:45:58 +0300

    add getListOfImages() to apiConnector

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -99,6 +99,43 @@ class ApiConnector {
       console.error(err);
     }
   }
+
+  // Implementation of /rekognition/list
+  // Returns list (or part) of administrators
+  // Required fields - token
+  // Optional fields - mode, amount, page
+  // TODO: Implement pagination using optional params
+  async getListOfImages(token, mode, amount, page) {
+    // Request url
+    const url = BACKEND_URL + '/rekognition/list';
+    // Check token
+    if (typeof(token) != 'string' || token.length <= 0) {
+      return {
+        status_code: -1,
+        error: 'What are you doing? Token is missing.'
+      };
+    }
+
+    try {
+      // Send request
+      const response = await axios.get(url, {
+        params: { token }
+      });
+
+      // Check response and return data
+      if (response.status == 200) {
+        return response.data;
+      } else {
+        return {
+          status_code: -1,
+          error: 'Something went wrong... Server returned code '
+                  + response.status
+        };
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
 
 export default new ApiConnector();
