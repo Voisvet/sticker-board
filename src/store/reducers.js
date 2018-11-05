68b03815b0e1485390b099be8e5217498867d258
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2018-11-05 11:47:19 +0300

    combine reducers in one export

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,11 @@
 import user from './user/reducer';
+import admins from './admins/reducer';
+import messages from './messages/reducer';
+import rekognition from './rekognition/reducer';
 
 export {
-  user
+  user,
+  admins,
+  messages,
+  rekognition
 };
