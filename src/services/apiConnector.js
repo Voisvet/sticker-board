f24bc01aa1eb9431f96dc80b24c7c33cef0c0ddb
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2018-11-05 10:37:07 +0300

    add getListOfMessages() to apiConnector

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 class ApiConnector {
+
+  // Implementation of /login request
+  // Authentication and authorization of user
+  // Required fields - login, password
   async login(login, password) {
     const url = BACKEND_URL + '/login';
     try {
@@ -9,6 +13,43 @@ class ApiConnector {
       });
       if (response.status == 200) {
         return response.data;
+      } else {
+        return {
+          status_code: -1,
+          error: 'Something went wrong... Server returned code '
+                  + response.status
+        };
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // Implementation of /messages/list request
+  // Returns list (or part) of messages
+  // Required fields - token
+  // Optional fields - mode, amount, page
+  // TODO: Implement pagination using optional params
+  async getListOfMessages(token, mode, amount, page) {
+    const url = BACKEND_URL + '/messages/list';
+    if (typeof(token) != 'string' || token.length <= 0) {
+      return {
+        status_code: -1,
+        error: 'What are you doing? Token is missing.'
+      };
+    }
+    try {
+      const response = await axios.get(url, {
+        params: { token }
+      });
+      if (response.status == 200) {
+        return response.data;
+      } else {
+        return {
+          status_code: -1,
+          error: 'Something went wrong... Server returned code '
+                  + response.status
+        };
       }
     } catch (err) {
       console.error(err);
