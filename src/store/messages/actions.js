710a758a02c1d583e4af37d220e0a4e154f09090
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2018-11-05 11:09:18 +0300

    add basic stuff connected with messages list fetching to store

---
+++ src/store/messages/actions.js
@@ -0,0 +1,30 @@
+import api from '../../services/apiConnector';
+
+import * as types from './actionTypes';
+import * as selectors from './reducer';
+
+export function fetchListOfMessages() {
+  return async(dispatch, getState) => {
+    // Notify that fetch started
+    dispatch({
+      type: types.TOKEN_FETCH_STARTED
+    });
+
+    const token = getState().user.token;
+    // Send request to server
+    const resp = await api.getListOfMessages(token);
+    if (resp.status_code == 0) {
+      // If all is alright, update messages in store
+      dispatch({
+        type: types.MESSAGES_FETCHED,
+        list: resp.list
+      });
+    } else {
+      // If something went wrong, update message in store
+      dispatch({
+        type: types.TOKEN_FETCH_FAILED,
+        errorMessage: resp.error
+      });
+    }
+  };
+}
---
+++ src/store/messages/reducer.js
@@ -0,0 +1,51 @@
+import * as types from './actionTypes';
+
+// Initialization of this part of store
+
+const initialState = {
+  list: undefined,
+  errorMessage: undefined,
+  fetchingInProgress: false
+};
+
+// Reducer for this part of store
+
+export default function reduce(state = initialState, action = {}) {
+  switch (action.type) {
+    case types.MESSAGES_FETCHED:
+      // TODO: Implement updating list of messages when
+      // will be implementing pagination
+      return {
+        ...state,
+        list: action.list,
+        fetchingInProgress: false
+      };
+    case types.MESSAGES_FETCH_STARTED:
+      return {
+        ...state,
+        fetchingInProgress: true
+      };
+    case types.MESSAGES_FETCH_FAILED:
+      return {
+        ...state,
+        fetchingInProgress: false,
+        errorMessage: action.errorMessage
+      };
+    default:
+      return state;
+  }
+}
+
+// Selectors for this part of store
+
+export function getListOfMessages(state) {
+  return state.messages.token;
+}
+
+export function getFetchingState(state) {
+  return state.messages.fetchingInProgress;
+}
+
+export function getErrorMessage(state) {
+  return state.messages.errorMessage;
+}
