fd72f9c86864a689e037a2e8452c467151233f5f
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2018-11-05 10:43:39 +0300

    add getListOfAdmins() to apiConnector

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -11,6 +11,7 @@ class ApiConnector {
       const response = await axios.get(url, {
         params: { login, password }
       });
+
       if (response.status == 200) {
         return response.data;
       } else {
@@ -31,17 +32,60 @@ class ApiConnector {
   // Optional fields - mode, amount, page
   // TODO: Implement pagination using optional params
   async getListOfMessages(token, mode, amount, page) {
+    // Request url
     const url = BACKEND_URL + '/messages/list';
+    //Check token
     if (typeof(token) != 'string' || token.length <= 0) {
       return {
         status_code: -1,
         error: 'What are you doing? Token is missing.'
       };
     }
+
     try {
+      // Send request
       const response = await axios.get(url, {
         params: { token }
       });
+
+      // Check response and return data
+      if (response.status == 200) {
+        return response.data;
+      } else {
+        return {
+          status_code: -1,
+          error: 'Something went wrong... Server returned code '
+                  + response.status
+        };
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // Implementation of /admins/list
+  // Returns list (or part) of administrators
+  // Required fields - token
+  // Optional fields - mode, amount, page
+  // TODO: Implement pagination using optional params
+  async getListOfAdmins(token, mode, amount, page) {
+    // Request url
+    const url = BACKEND_URL + '/admins/list';
+    // Check token
+    if (typeof(token) != 'string' || token.length <= 0) {
+      return {
+        status_code: -1,
+        error: 'What are you doing? Token is missing.'
+      };
+    }
+
+    try {
+      // Send request
+      const response = await axios.get(url, {
+        params: { token }
+      });
+
+      // Check response and return data
       if (response.status == 200) {
         return response.data;
       } else {
