7a1d80deaf543c802873bc830685c39290ca02ad
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2018-11-06 13:48:15 +0300

    change styling a little bit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import App from './App';
 
@@ -13,9 +14,32 @@ const store = createStore(
   applyMiddleware(thunk)
 );
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#7bddff',
+      main: '#3eabe3',
+      dark: '#007cb1',
+      contrastText: '#ffffff'
+    },
+    secondary: {
+      light: '#ff8890',
+      main: '#e85663',
+      dark: '#b01f39',
+      contrastText: '#7f0000'
+    },
+    // error: will use the default color
+    typography: {
+      useNextVariants: true
+    }
+  }
+});
+
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <MuiThemeProvider theme={theme}>
+      <App/>
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('app')
 );
