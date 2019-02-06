fc137e9121642f683ad5d7a333aa38bf22ec161f
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2019-02-06 14:54:24 +0300

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  testEnvironment: "node"
+  testEnvironment: "node",
+  globals: {
+    "BACKEND_URL": "http://localhost:8090/api"
+  }
 };
--- src/__tests__/pages/ChatChips.test.js
+++ src/__tests__/pages/ChatChips.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import ChatsChips from '../../pages/Main/ListMessagePage/MessageInfoModal/ChatsChips';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 test("Chat chips render correctly", () => {
   const testData = ["Chat name 1", "Chat name 2", "Another chat name..."];
 
-  const component = renderer.create(<ChatsChips chats={testData}/>).toJSON();
+  const component = Enzyme.shallow(<ChatsChips chats={testData}/>);
   expect(component).toMatchSnapshot();
 });
